b09fdd2faec6feb3eda0db913ee88e829539636b
LoreGerm <lorenzogermano146@gmail.com>
2022-03-09 09:53:48 +0100

    .

---
+++ sa_ca_fo/script.js
@@ -0,0 +1,18 @@
+function start(scelta){
+
+    let risultato = Math.floor(Math.random() * 3);
+    alert(risultato);
+    const SASSO = 0;
+    const FORBI = 1;
+    const CARTA = 2;
+
+    if(risultato == SASSO){
+
+    }
+    else if(risultato == FORBI){
+
+    }
+    else{
+
+    }
+}
