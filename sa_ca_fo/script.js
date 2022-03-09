2406f9908237a571304c8432ebe12c6a0c99ea9b
LoreGerm <lorenzogermano146@gmail.com>
2022-03-09 10:16:40 +0100

    .

--- sa_ca_fo/script.js
+++ sa_ca_fo/script.js
@@ -1,18 +1,51 @@
 function start(scelta){
 
+    document.getElementById('home').classList.remove('d-none');
+    document.getElementById('gioco').classList.add('d-none');
+
     let risultato = Math.floor(Math.random() * 3);
-    alert(risultato);
     const SASSO = 0;
     const FORBI = 1;
     const CARTA = 2;
 
-    if(risultato == SASSO){
+    if(scelta == SASSO){
+        if(risultato == SASSO){
+            document.getElementById('ris').innerHTML = '<img src="img/sasso.png" alt=""> <p> PAREGGIO </p>';
+        }
+        else if(risultato == FORBI){
+            document.getElementById('ris').innerHTML = '<img src="img/forbice.png" alt=""> <p> VINTO </p>';
+        }
+        else{
+            document.getElementById('ris').innerHTML = '<img src="img/carta.png" alt=""> <p> PERSO </p>';
+        }
+    }
+    else if(scelta == FORBI){
+        if(risultato == SASSO){
+            document.getElementById('ris').innerHTML = '<img src="img/sasso.png" alt=""> <p> PERSO </p>';
+        }
+        else if(risultato == FORBI){
+            document.getElementById('ris').innerHTML = '<img src="img/forbice.png" alt=""> <p> PAREGGIO </p>';
+        }
+        else{
+            document.getElementById('ris').innerHTML = '<img src="img/carta.png" alt=""> <p> VINTO </p>';
+        }
+    }
+    else if(scelta == CARTA){
+        if(risultato == SASSO){
+            document.getElementById('ris').innerHTML = '<img src="img/sasso.png" alt=""> <p> VINTO </p>';
+        }
+        else if(risultato == FORBI){
+            document.getElementById('ris').innerHTML = '<img src="img/forbice.png" alt=""> <p> PERSO </p>';
+        }
+        else{
+            document.getElementById('ris').innerHTML = '<img src="img/carta.png" alt=""> <p> PAREGGIO </p>';
+        }
 
     }
-    else if(risultato == FORBI){
+}
 
-    }
-    else{
+function home(){
 
-    }
+    document.getElementById('home').classList.add('d-none');
+    document.getElementById('gioco').classList.remove('d-none');    
 }
